105d01e61b0c2efbffc6c1a51bbd4ca31b702931
Oleg Shevtsov <olegshevtsov1@gmail.com>
2020-08-26 17:16:05 +0300

    Task/RG-5-codebreaker-web: add module validate

--- controllers/register_game.rb
+++ controllers/register_game.rb
@@ -1,19 +1,13 @@
 # frozen_string_literal: true
 
 class RegisterGame
+  include Validator
+
   def create_game(request)
     player = CodebreakerOs::Player.new(request.params['player_name'])
     validate(player)
 
     difficulty = CodebreakerOs::Difficulty.new(request.params['level'])
     @game = CodebreakerOs::Game.new(player, difficulty)
-  end
-
-  def error_name
-    raise StandardError, "Name #{request.params['player_name']} isn't valid"
-  end
-
-  def validate(player)
-    error_name unless player.valid?
   end
 end
